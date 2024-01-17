235683363ee5bc534412cb8cd64dfb88a3bfa63f
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2024-01-17 16:41:27 -0300

    feat: include information on Java exceptions

---
+++ Fundamentos_java/excecoes/Excecoes.java
@@ -0,0 +1,29 @@
+package Fundamentos_java.excecoes;
+
+public class Excecoes {
+  // lidar com erros
+
+  // checked exceptions - nós conseguimos prever e  tratar
+
+  // unchecked exceptions - excessões nas quais não temos controle
+
+  public static void main(String[] args) throws Exception {
+    // validarNumero();
+    // try and catch
+    try {
+      validarNumero();
+    } catch (Exception e) {
+     System.out.println("Quebrou");
+     e.printStackTrace();
+    }
+    }
+
+    // o static permite seu método seja acessado sem ser instanciado
+
+    public static void validarNumero() throws Exception {
+      int numero = 10;
+      if(numero < 100) {
+        throw new Exception("O número é menor que 100");
+    }
+  }
+}
