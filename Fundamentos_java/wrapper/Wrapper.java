4a8820bd505fe9b768d0fa480f736b0b54a460eb
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2024-01-19 10:28:05 -0300

    feat: include information on Java wrapper classes

---
+++ Fundamentos_java/wrapper/Wrapper.java
@@ -0,0 +1,14 @@
+package wrapper;
+
+public class Wrapper {
+  public static void main(String[] args) {
+    int tipoInt = 1;
+    float tipoFloat = 1;
+    boolean tipoBoolean = false;
+
+    // para utilizar os wrappers so basta transformalos em maiúsculas com excessão do int e do char
+    Float tipoFloat2 = 10F;
+    Boolean tipoBoolean2 = false;
+    Integer tipoInteger = 1;
+  }
+}
